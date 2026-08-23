99e376a3c738c39b141c3fa4ac0983c307294199
hasnat-ahmed-hasib <2024831050@student.sust.edu>
2026-08-23 20:47:24 +0600

    Added data types and Loops.

---
+++ basic/dataTypes.java
@@ -0,0 +1,14 @@
+public class dataTypes{
+    public static void main(String[] args){
+        byte age=23;
+        int balance=10000;
+        float pi=3.14159F;
+        double value_of_e=2.71828;
+        char firstAlphabet='a';
+
+        //constant
+        final float PI=3.14159F;
+        //PI=1.111F;->It will show error
+        System.err.println("Value of PI:" + PI);
+    }
+}
---
+++ basic/loops.java
@@ -0,0 +1,20 @@
+public class loops{
+    public static void main(String[] args) {
+        //FOR LOOP
+        for(int i=0;i<10;i++){
+            System.out.println("FOR loop: "+i);
+        }
+        //while loop
+        int i=0;
+        while(i<10){
+            System.out.println("While Loop: "+ i);
+            i++;
+        }
+        //do-while loop
+        i=0;
+        do { 
+            System.out.println("Do Wile Loop: "+ i);
+            i++;
+        } while (i<10);
+    }
+}
